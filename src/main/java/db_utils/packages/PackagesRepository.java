1b0a233e1afce1d45699bbc3e1aecd68e92a7e9e
Dominik <m3533077@nwytg.net>
2019-01-12 02:39:21 +0100

    Implemented whole Employee controller

--- src/main/java/db_utils/packages/PackagesRepository.java
+++
@@ -1,9 +0,0 @@
-package db_utils.packages;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PackagesRepository extends JpaRepository<Packages, Long>
-{
-}
--- src/main/java/db_utils/storage_type/StorageTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package db_utils.storage_type;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StorageTypeRepository extends JpaRepository<StorageType, Long>
-{
-}
