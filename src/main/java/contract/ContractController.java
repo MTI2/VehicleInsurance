39791e4a30ea148a9372ddefe7b05dfcb5f93383
Dominik <m3533077@nwytg.net>
2019-01-15 23:44:38 +0100

    Zmergowałem wasze wypociny.

--- src/main/java/contract/ContractController.java
+++ src/main/java/contract/ContractController.java
@@ -5,23 +5,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import db_utils.contract.Contract;
 import db_utils.contract.ContractRepository;
 import employee.Session;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.SecureRandom;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.sql.Date;
 
 public class ContractController {
     final String extApiUrl = "http://51.145.158.146:4040";
